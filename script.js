dc9d46a175bfae19ca6dfb77cd3b763b945a98fa
khalid3886 <142919731+khalid3886@users.noreply.github.com>
2024-02-02 02:33:54 +0530

    added resume

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function toggleMenu() {
   function openLinkInNewTab(url) {
     window.open(url, '_blank');
 }
+
+document.getElementById("resume-link-1").addEventListener('click',()=>{
+  window.open("https://drive.google.com/file/d/15e-alzZDHghqxxXY8bCtB-P2npnRb1cr/view?usp=drive_link")
+})
+
+document.getElementById("resume-link").addEventListener('click',()=>{
+  window.open("https://drive.google.com/file/d/15e-alzZDHghqxxXY8bCtB-P2npnRb1cr/view?usp=drive_link")
+})
+
+document.getElementById("resume-button-2").addEventListener('click',()=>{
+  var link = document.createElement('a');
+  link.href = './assets/portfolio.pdf';
+  link.target = '_blank';
+  link.download = 'resume.pdf';
+  link.click();
+  window.open("https://drive.google.com/file/d/15e-alzZDHghqxxXY8bCtB-P2npnRb1cr/view?usp=drive_link")
+})
+document.getElementById("resume-button").addEventListener('click',()=>{
+  var link = document.createElement('a');
+  link.href = './assets/portfolio.pdf';
+  link.target = '_blank';
+  link.download = 'resume.pdf';
+  link.click();
+  window.open("https://drive.google.com/file/d/15e-alzZDHghqxxXY8bCtB-P2npnRb1cr/view?usp=drive_link")
+})
